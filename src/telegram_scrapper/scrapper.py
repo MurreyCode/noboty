9fe0b20193bbbf7d48441ed341e59b58fcdc1f23
Pablo Reynel <pablo.reynel@gmail.com>
2025-02-06 21:04:08 +0100

    refactor: load env creds

--- src/telegram_scrapper/scrapper.py
+++ src/telegram_scrapper/scrapper.py
@@ -1,8 +1,12 @@
+import os
+import dotenv
 from telethon.sync import TelegramClient
 
+dotenv.load_dotenv()
+
 SESSION_FILE = "my_session"
-API_ID = ""
-API_HASH = ""
+API_ID = os.getenv("API_ID")  # Get from environment
+API_HASH = os.getenv("API_HASH")  # Get from environment
 CHANNEL_USERNAME = "@nowhereNUDES"
 
 if __name__ == "__main__":
